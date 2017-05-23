bc27c153675f0ee66c2aff5bd20fe742ad4c2e56
Franklin Chen <franklinchen@franklinchen.com>
2017-05-23 18:56:14 -0400

    Fix test, add new one.

--- test_check_chat_media.py
+++ test_check_chat_media.py
@@ -17,12 +17,12 @@ relative_chat_path = 'a/b/c/file.cha'
 
 media_root_dir = '/media'
 
-media_set = set([
-    '/media/a/b/x.mp3',
-    '/media/a/b/c/y.mp4',
-    '/media/a/b/c/j1.jpg',
-    '/media/a/b/c/j2.jpg'
-])
+media_dict = {
+    '/media/a/b/c/x.mp3': '/media/a/b/c/X.mp3',
+    '/media/a/b/c/y.mp4': '/media/a/b/c/y.mp4',
+    '/media/a/b/c/j1.jpg': '/media/a/b/c/j1.jpg',
+    '/media/a/b/c/j2.jpg': '/media/a/b/c/j2.jpg',
+}
 
 many_text = """
 @Media:\ty, video
@@ -32,6 +32,10 @@ many_text = """
 missing_video_text = """
 @Media:\tx, video
 %pic:"j1.jpg" junk %pic:"j2.jpg"
+"""
+
+wrong_case_text = """
+@Media:\tx, audio
 """
 
 
@@ -44,10 +48,16 @@ def test_parse_chat_doc_medias():
 
 
 def test_chat_doc_errors_good():
-    assert chat_doc_errors(relative_chat_path, many_text, media_root_dir, media_set) == []
+    assert chat_doc_errors(relative_chat_path, many_text, media_root_dir, media_dict) == []
 
 
 def test_chat_doc_errors_missing_video():
-    assert chat_doc_errors(relative_chat_path, missing_video_text, media_root_dir, media_set) == [
+    assert chat_doc_errors(relative_chat_path, missing_video_text, media_root_dir, media_dict) == [
         ('missing', 'video', '/media/a/b/c/x.mp4')
+    ]
+
+
+def test_chat_doc_wrong_case():
+    assert chat_doc_errors(relative_chat_path, wrong_case_text, media_root_dir, media_dict) == [
+        ('incorrectly-cased /media/a/b/c/X.mp3', 'audio', '/media/a/b/c/x.mp3')
     ]
