107b9a76a53822b15a1d39a3c9d75a98bec2c3f6
Brian MacWhinney (brian) <macw@cmu.edu>
2017-06-09 10:10:27 -0400

    removed my batch file

--- check-all.bat
+++
@@ -1,9 +0,0 @@
-check_chat_media --chatdir ../0aphasia-data --host talkbank.org --mediadir /TalkBank/Aphasia/media
-check_chat_media --chatdir ../0childes-data --host childes.talkbank.org --mediadir /web/childes/media
-check_chat_media --chatdir ../0fluency-data --host childes.talkbank.org --mediadir /web/FluencyBank/media
-check_chat_media --chatdir ../0phon-data --host childes.talkbank.org --mediadir /web/PhonBank/media
-check_chat_media --chatdir ../0rhd-data --host talkbank.org --mediadir /TalkBank/RHDBank/media
-check_chat_media --chatdir ../0samtale-data --host talkbank.org --mediadir /TalkBank/SamtaleBank/media
-check_chat_media --chatdir ../0tbi-data --host talkbank.org --mediadir /TalkBank/TBIBank/media
-% check_chat_media --chatdir ../0talkbank-data --host talkbank.org --mediadir /TalkBank/media
-
